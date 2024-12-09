d75fb9d4c4c303f17fab76519fcb029f95304d41
Sara <sala0002@stud.kea.dk>
2024-12-09 14:09:42 +0100

    forside færdig

--- src/app/page.js
+++ src/app/page.js
@@ -3,21 +3,26 @@ import React from "react";
 import TopNav from "@/components/TopNav";
 import Footer from "@/components/Footer";
 import BottomNav from "../components/BottomNav";
-
-
+import FrontPageHeader from "@/components/FrontPageHeader";
 
 export default function Home() {
   return (
     <div className="layout-wrapper">
-    <div className="hidden md:block">
-      <TopNav />
-    </div>
-    <Footer />
+      {/* top navigation (kun synlig på desktop) */}
+      <div className="hidden md:block">
+        <TopNav />
+      </div>
 
- 
-    <div className="block md:hidden">
-      <BottomNav />
+      {/* forsidens hovedindhold */}
+      <FrontPageHeader />
+
+      {/* footer */}
+      <Footer />
+
+      {/* bottom navigation (kun synlig på mobil) */}
+      <div className="block md:hidden">
+        <BottomNav />
+      </div>
     </div>
-  </div>
   );
 }
