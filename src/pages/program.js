49b7c152fc855bab439a695b6d5650c17c8d6fee
Maria <madr0002@stud.kea.dk>
2024-12-10 11:34:29 +0100

    Program start, skal merge med main

---
+++ src/pages/program.js
@@ -0,0 +1,11 @@
+"use client";
+import Banner from "@/components/Banner";
+import React from "react";
+
+const Program = () => {
+    return ( <div>
+        <Banner text="PROGRAM"/>
+    </div> );
+}
+ 
+export default Program;
