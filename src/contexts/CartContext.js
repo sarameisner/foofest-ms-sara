0cee714a8b6abbbee19a063e26c3d543b545029b
Sara <sala0002@stud.kea.dk>
2024-12-11 17:08:29 +0100

    styling af basket.js

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,25 +1,37 @@
+// til hooks og kontekst
 "use client";
 
 import React, { createContext, useState, useEffect } from "react";
 
+// her opretter vi konteksten til delingen af vores data på tværs af komponenter
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
+  // startdata af billetterne
   const [tickets] = useState([
     { id: 1, name: "Regular Ticket", price: 799 },
     { id: 2, name: "VIP Ticket", price: 1299 },
   ]);
+  // gemmer de valgte billetter der er tilføjet til kurven
   const [cartItems, setCartItems] = useState([]);
+  // gemmer camping valget
   const [selectedCamping, setSelectedCamping] = useState(null);
+  // gemmer hvilket telt de skal sove i
   const [selectedOptional, setSelectedOptional] = useState([]);
+  // gemmer brugeroplysninger
   const [userInfo, setUserInfo] = useState({
     name: "",
     email: "",
     birthday: "",
   });
+  // samlet pris
   const [cartTotal, setCartTotal] = useState(0);
 
-  // Tilføj en vare til kurven
+  // tilføjer en vare til kurven
+  // hvis varen allerede findes opdaterer vi antallet
+  // eller tilføjer den som ny vare
+  // @param {Object} product - er varen der skal tilføjes
+
   const addToCart = (product) => {
     setCartItems((prevItems) => {
       const existingItem = prevItems.find((item) => item.id === product.id);
@@ -31,28 +43,29 @@ export const CartProvider = ({ children }) => {
     });
   };
 
-  // Opdater mængde af en vare i kurven
+  // konstanten for at opdatere antal
   const updateItemQuantity = (id, quantity) => {
     setCartItems((prevItems) => prevItems.map((item) => (item.id === id ? { ...item, quantity: Math.max(1, quantity) } : item)));
   };
 
-  // Fjern en vare fra kurven
+  // fjerner item  baseret på id
   const removeFromCart = (id) => {
     setCartItems((prevItems) => prevItems.filter((item) => item.id !== id));
   };
 
-  // Beregn den samlede pris
+  // beregnelse af den totale pris
   const calculateCartTotal = () => {
     const ticketTotal = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
     const optionalTotal = selectedOptional.reduce((total, item) => total + item.price * item.quantity, 0);
     setCartTotal(ticketTotal + optionalTotal);
   };
 
-  // Opdater totalpris, når `cartItems` eller `selectedOptional` ændres
+  // opdaterer totalpris når `cartItems` eller `selectedOptional` ændres
   useEffect(() => {
     calculateCartTotal();
   }, [cartItems, selectedOptional]);
 
+  // her returnerer vi alt hvad der er nødvendigt for at have vores checkout flow
   return (
     <CartContext.Provider
       value={{
